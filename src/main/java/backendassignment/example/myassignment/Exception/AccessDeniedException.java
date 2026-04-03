8c5083111a2b7e62e7496ed5a5909df00775170b
vyshnavikanugula <161703447+vyshnavikanugula@users.noreply.github.com>
2026-04-03 10:32:54 +0530

    Delete src directory

--- src/main/java/backendassignment/example/myassignment/Exception/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package backendassignment.example.myassignment.Exception;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/backendassignment/example/myassignment/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package backendassignment.example.myassignment.repository;
-
-import backendassignment.example.myassignment.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
-
